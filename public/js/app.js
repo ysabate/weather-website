20f0ae9c56a6a63536230b8674530a579c88c857
Yabetse Abate <aa@Yabetse-MacBook-Pro.local>
2021-08-26 11:33:13 -0230

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+console.log('Client side javascript file is loaded!');
+
+
+
+const weatherForm = document.querySelector('form');
+const messageOne = document.getElementById('message-1');
+const messageTwo = document.getElementById("message-2");
+
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const address = document.getElementById("address").value;
+
+    messageOne.textContent = "LOADING...";
+    messageTwo.textContent = '';
+
+    fetch(`http://localhost:3000/weather?address=${address}`).then(
+      (response) => {
+        response.json().then((data) => {
+          if (data.error) {
+            messageOne.textContent = data.error;
+          }
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
+          
+        });
+      }
+    );
+
+})
+
+
---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=791224dff5317b7172cc26665252a6d1&query=${latitude},${longitude}`;
+
+  request({ url, json: true }, (error, {body}) => {
+    if (error) {
+      callback("Unable to connect to weather service", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      const description = body.current["weather_descriptions"][0];
+      const temperature = body.current.temperature;
+      const feelslike = body.current.feelslike;
+      callback(
+        undefined,
+        `${description}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
